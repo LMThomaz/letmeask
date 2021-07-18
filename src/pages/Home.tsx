c3cd747a7b4a0b472a3226e5a2dc2ca8f58710c1
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-07-18 17:55:52 -0300

    Login with Google in context api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
+import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import { AuthContext } from '../App';
 import googleIconImg from '../assets/images/google-icon.svg';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -7,8 +9,11 @@ import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useContext(AuthContext);
 
-  function navigateToNewRoom() {
+  async function handleCreateRoom() {
+    if (!user) await signInWithGoogle();
+
     history.push('/rooms/new');
   }
 
@@ -25,7 +30,7 @@ export function Home() {
       <main>
         <div className='main-content'>
           <img src={logoImg} alt='Letmeask' />
-          <button className='create-room' onClick={navigateToNewRoom}>
+          <button className='create-room' onClick={handleCreateRoom}>
             <img src={googleIconImg} alt='logo do google' /> Crie sua sala com o
             Google
           </button>
