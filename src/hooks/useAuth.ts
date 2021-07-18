f73c513ebc3dab03715d15267d0e4b83c0443ad1
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-07-18 18:17:09 -0300

    create hook to context

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,7 @@
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContexts';
+
+export function useAuth() {
+  const value = useContext(AuthContext);
+  return value;
+}
